d3a0ef540f8f909cae2d4a7d9e59d264fc343515
Justus Ip <justusip@yahoo.com.hk>
2023-04-26 01:14:40 +0800

    hmm

--- src/definitions.py
+++ src/definitions.py
@@ -1,4 +1,4 @@
 import os
 from pathlib import Path
 
-root_dir = Path(os.path.dirname(os.path.abspath(__file__))) / ".."
+root_dir = (Path(os.path.dirname(os.path.abspath(__file__))) / "..").resolve()
