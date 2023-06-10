6c880c9e59a7238a3734b0cb400782a0e7325e8c
Justus Ip <justusip@yahoo.com.hk>
2023-06-10 17:28:31 +0800

    hmm

--- src/train.py
+++ src/train.py
@@ -1,30 +1,15 @@
+import argparse
 import datetime
 import os
 import shutil
-import sys
 from pathlib import Path
 
 from ultralytics import YOLO
 
 from src.definitions import root_dir
 
-if __name__ == '__main__':
-    dir_output = (root_dir / "output").resolve()
-    if not dir_output.exists():
-        print(f"{dir_output} does not exist.")
-        exit()
 
-    dir_datasets = [
-        (dir_output / "dataset_locate", 50),
-        (dir_output / "dataset_identify_letters", 10),
-        (dir_output / "dataset_identify_shapes", 10),
-    ]
-
-    for p, s in dir_datasets:
-        if not p.exists():
-            print(f"{p} does not exist.")
-            exit()
-
+def clean():
     print("Deleting previously trained weights...")
     for p, s in dir_datasets:
         dir_runs_train = p / "runs"
@@ -36,18 +21,14 @@ if __name__ == '__main__':
         shutil.rmtree(dir_runs_train)
 
 
-    def train(dir_dataset: Path, imgsz: int):
-        os.chdir(str(dir_dataset))
-        path_data_yaml = (dir_dataset / "data.yaml").resolve()
-        model = YOLO('yolov8n.pt')
-        model.train(data=str(path_data_yaml), epochs=100, imgsz=imgsz, cache="ram")
+def train(dir_dataset: Path, imgsz: int):
+    os.chdir(str(dir_dataset))
+    path_data_yaml = (dir_dataset / "data.yaml").resolve()
+    model = YOLO('yolov8n.pt')
+    model.train(data=str(path_data_yaml), epochs=100, imgsz=imgsz, cache="ram")
 
 
-    if not "--extract" in sys.argv:
-        for p, s in dir_datasets:
-            print(f"Training {p}...")
-            train(p, s)
-
+def export():
     dir_weights_output = root_dir / "output" / "weights"
     for p, s in dir_datasets:
         dir_runs_detect = p / "runs" / "detect"
@@ -68,3 +49,44 @@ if __name__ == '__main__':
 
         print(f"Copying {path_best_pt} to {dir_weights_output_dated}...")
         shutil.copy(path_best_pt, dir_weights_output_dated / (p.stem + ".pt"))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Train the dataset prepared with src.generate and src.convert using Ultralytics YOLOv8 and export '
+                    'them as .pt weights file under output/weights')
+
+    parser.add_argument('--clean-only', "-c", action='store_true',
+                        help='Delete previously trained weights ("runs" directories) only')
+    parser.add_argument('--export-only', "-e", action='store_true',
+                        help='Export the weights only after training (After .pt are generated in each dataset directory)')
+    parser.add_argument("--exclude", "-t", action="append",
+                        help="Exclude a dataset from cleaning, training and exporting (Specify the dataset directory name, e.g., dataset_locate)")
+
+    args = parser.parse_args()
+
+    dir_output = (root_dir / "output").resolve()
+    if not dir_output.exists():
+        print(f"{dir_output} does not exist.")
+        exit()
+
+    dir_datasets = [
+        (dir_output / "dataset_locate", 64),
+        (dir_output / "dataset_identify_letters", 16),
+        (dir_output / "dataset_identify_shapes", 16),
+    ]
+    if args.exclude:
+        for n in args.exclude:
+            if n not in [p.stem for p, s in dir_datasets]:
+                print(f"Dataset {n} does not exist.")
+                exit()
+            dir_datasets = [(p, s) for p, s in dir_datasets if p.stem not in args.exclude]
+
+    for p, s in dir_datasets:
+        if not p.exists():
+            print(f"{p} does not exist.")
+            exit()
+
+    for p, s in dir_datasets:
+        print(f"Training {p}...")
+        train(p, s)
